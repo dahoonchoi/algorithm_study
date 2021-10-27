dc2b71872058007c880508a93799b46b9f43a467
dahoonchoi <ekgns8258@naver.com>
2021-10-27 23:02:55 +0900

    Algorithm Study

---
+++ hackerrank/climbing_the_leaderboard.py
@@ -0,0 +1,25 @@
+# 하나의 랭킹 점수판이 주어진다
+# 주어진 점수판에서 플레이어가 받은 점수의 추가로 순위를 출력하시오
+
+# input
+# ranked : 점수판
+# player : 플레이어 점수
+
+import math
+import os
+import random
+import re
+import sys
+
+def climbingLeaderboard(ranked, player):
+
+    ranked.extend(player)
+    buff = sorted(ranked,reverse=True)
+    print(buff)
+    for p in player:
+        print(buff.index(p)+1)
+
+ranks = [100,90,90,80,75,60]
+player = [50,65,77,90,102]
+
+climbingLeaderboard(ranks,player)
---
+++ hackerrank/picking_numbers.py
@@ -0,0 +1,30 @@
+# 배열이 주어지고 두개의 차가 1보다 작거나 1과 같은 값은 갯수를
+# 출력하시오
+
+# input
+# array
+
+# output
+# 1과 같거나 작은 갯수
+
+import math
+import os
+import random
+import re
+import sys
+
+def pickingNumbers(a):
+    answer = 0
+    
+    for i in a:
+    # find 1 or 0 value
+       c = a.count(i)
+       d = a.count(i-1)
+
+       if c+d > answer:
+           answer = c+d
+
+    return answer
+
+a = [4,6,5,3,3,1]
+pickingNumbers(a)
